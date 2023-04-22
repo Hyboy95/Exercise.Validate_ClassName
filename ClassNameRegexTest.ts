17739c00ba4527342b6ac0b669203a0e274a9147
Hyboy95 <temilator@gmail.com>
2023-04-22 12:03:25 +0700

    second commit

--- ClassNameRegexTest.ts
+++ ClassNameRegexTest.ts
@@ -7,9 +7,9 @@ let invalidClassName: string[] = ["C0_223G", "B3456H", "P0112A"];
 
 for (let className of validClassName) {
     let isvalid: boolean = classNameRegex.validate(className);
-    console.log(`Email is ${className} is valid: ${isvalid}`)
+    console.log(`ClassName is ${className} is valid: ${isvalid}`)
 }
 for (let className of invalidClassName) {
     let isvalid: boolean = classNameRegex.validate(className);
-    console.log(`Email is ${className} is valid: ${isvalid}`)
+    console.log(`ClassName is ${className} is valid: ${isvalid}`)
 }
